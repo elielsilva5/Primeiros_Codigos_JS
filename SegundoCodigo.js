4c0dcb3b5c83d88747a1218c4ea7f936fe563fa9
elielsilva5 <elielsilva.eng@gmail.com>
2024-05-15 13:12:31 -0300

    Update SegundoCodigo.js

--- SegundoCodigo.js
+++ SegundoCodigo.js
@@ -1,6 +1,10 @@
-console.log("Olá mundo! Meu nome é Eliel.");
-console.log("Olá mundo! Meu nome é Eliel.");
-console.log("Olá mundo! Meu nome é Eliel.");
-console.log("Olá mundo! Meu nome é Eliel.");
-console.log("Olá mundo! Meu nome é Eliel.");
-console.log("Olá mundo! Meu nome é Eliel.");
+// const mensagemTela = "Olá mundo! Meu nome é Eliel.";
+let mensagemTela = "Bora aprender?";
+console.log(mensagemTela);
+console.log(mensagemTela);
+console.log(mensagemTela);
+console.log(mensagemTela);
+
+mensagemTela = "Bora! Estou empolgado em aprender JavaScript!";
+
+console.log(mensagemTela);
