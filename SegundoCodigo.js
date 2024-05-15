1ae40b7bd6607ba9ae0c03afad83cc01447a9719
elielsilva5 <elielsilva.eng@gmail.com>
2024-05-15 12:56:15 -0300

    Create SegundoCodigo.js

---
+++ SegundoCodigo.js
@@ -0,0 +1,6 @@
+console.log("Olá mundo! Meu nome é Eliel.");
+console.log("Olá mundo! Meu nome é Eliel.");
+console.log("Olá mundo! Meu nome é Eliel.");
+console.log("Olá mundo! Meu nome é Eliel.");
+console.log("Olá mundo! Meu nome é Eliel.");
+console.log("Olá mundo! Meu nome é Eliel.");
